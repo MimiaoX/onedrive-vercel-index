57e84f64d637dfd8c97e9567f9230ea2d37d406d
MimiaoX <3192062061119@glut.edu.cn>
2022-03-24 12:36:29 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -35,7 +35,7 @@ module.exports = {
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
   footer:
-    'Copyrights © 2022 <a href="https://x-lac.vercel.app/">MimiaoX</a> - <a href="https://x-lac.vercel.app/1">免责声明</a>',
+    'Copyrights © 2022 MimiaoX',
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
@@ -51,10 +51,6 @@ module.exports = {
     {
       name: 'GitHub',
       link: 'https://github.com/MimiaoX',
-    },
-    {
-      name: 'blog-Solid',
-      link: 'https://x-lac.vercel.app',
     },
          ],
 
